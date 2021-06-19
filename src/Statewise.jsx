1026d157a433bd2c9e2497c33ec268f565c298d6
Arjunpatidar9754 <67516896+Arjunpatidar9754@users.noreply.github.com>
2021-06-20 00:46:17 +0530

    1 commit

---
+++ src/Statewise.jsx
@@ -0,0 +1,57 @@
+import React, {useState, useEffect} from 'react';
+
+const StateWise = () => {
+	const [data, setdata] = useState([]);
+
+	const getCovidData = async () => {
+		const res = await fetch('https://api.covid19india.org/data.json');
+		const actualData = await res.json();
+		
+		setdata(actualData.statewise);
+	}
+	console.log(data);
+
+	useEffect(() =>{
+		getCovidData();
+	}, []);
+	return(
+		<>
+		<h1> StateWise Data </h1>
+		<div>
+			<div>
+				<table border="2px">
+					<thead>
+						<tr>
+							<th>State</th>
+							<th>Confirmed</th>
+							<th>recovered</th>
+							<th>deaths</th>
+							<th>active</th>
+							<th>lastupdatedtime</th>
+						</tr>
+					</thead>
+					<tbody>
+						{
+							data.map((currEle,index) =>{
+								return(
+									<tr>
+										<th>{currEle.state}</th>
+										<td>{currEle.confirmed}</td>
+										<td>{currEle.recovered}</td>
+										<td>{currEle.deaths}</td>
+										<td>{currEle.active}</td>
+										<td>{currEle.lastupdatedtime}</td>
+									</tr>
+									);
+							})
+						}
+						
+					</tbody>
+				</table>
+			</div>
+		</div>
+		</>
+		);
+}
+
+export default StateWise;
