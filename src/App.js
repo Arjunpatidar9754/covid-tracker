850d8d57517a6308c252ec16fa70a35881041c75
Arjunpatidar9754 <67516896+Arjunpatidar9754@users.noreply.github.com>
2021-06-20 22:04:01 +0530

    4rt

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import './App.css';
-import Statewise from './Statewise.jsx';
+import {Route, Switch} from 'react-router';
+import StateWise from './Statewise.jsx';
+import Header from './html/Header.jsx';
+import About from './html/About.jsx';
+import Vaccination from './html/Vaccination.jsx';
+
 
 function App() {
   return (
     <>
-    <div className="App">
-      <Statewise />
-    </div>
+    <Header />
+    <Switch>
+      <Route exact path="/about" component={About} />
+      <Route exact path="/vaccination" component={Vaccination} />
+      <Route exact path="/" component={StateWise} />
+    </Switch>
     </>
+    
   );
 }
-
 export default App;
---
+++ src/html/About.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const About = () =>{
+
+	return(
+			<>
+			<br />
+			<br />
+			<br />
+			<h1> Hellow About Us!!!!</h1>
+			</>
+		);
+}
+
+export default About;
