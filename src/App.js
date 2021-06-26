42ed79f47bc42de2834cc27c3c2ad2bc6b224d96
Arjunpatidar9754 <67516896+Arjunpatidar9754@users.noreply.github.com>
2021-06-26 17:16:57 +0530

    Revert "vaccine tracker"
    This reverts commit 4ba607b1b88bfa3a47033378c5f1b90c8410f6cb.

--- src/App.js
+++ src/App.js
@@ -6,8 +6,6 @@ import About from './html/About.jsx';
 import Vaccination from './html/Vaccination.jsx';
 import Home from './html/Home.jsx';
 import Footer from './html/Footer.jsx';
-import Pincode from './html/Pincode';
-import District from './html/District';
 
 
 function App() {
@@ -18,12 +16,8 @@ function App() {
       <Route exact path="/about" component={About} />
       <Route exact path="/vaccination" component={Vaccination} />
       <Route exact path="/statewise" component={StateWise} />
-      <Route path="/vaccination/pincode" component={Pincode} />
-      <Route path="/vaccination/district" component={District} />
-      <Route path="/vaccination/*" component={Vaccination} /> 
       <Route exact path="/" component={Home} />
-
-    </Switch>
+    </Switch><br /><br /><br /><br /><br /><br /><br /><br /><br />
     <Footer />
     </>
     
